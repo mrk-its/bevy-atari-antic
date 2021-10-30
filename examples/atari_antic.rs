d4750419210c9c8cc6c4b758a74d2ebbbe05d3cc
Mariusz Kryński <mrk@sed.pl>
2021-10-30 22:24:03 +0200

    change initialization of gtia data

--- examples/atari_antic.rs
+++ examples/atari_antic.rs
@@ -55,23 +55,61 @@ fn setup(
     let mut atari_data = AtariData::default();
     atari_data.insert_mode_line(104, 256, 8, 0, 0, 0, 1024, 0);
     atari_data.insert_mode_line(112, 256, 8, 2, 0, 0, 1024, 0);
-    atari_data.insert_mode_line(120, 256, 8, 2, 0, 0, 1024+40, 0);
-    atari_data.insert_mode_line(128, 256, 8, 2, 0, 0, 1024+80, 0);
+    atari_data.insert_mode_line(120, 256, 8, 2, 0, 0, 1024 + 40, 0);
+    atari_data.insert_mode_line(128, 256, 8, 2, 0, 0, 1024 + 80, 0);
     atari_data.insert_mode_line(136, 256, 8, 2, 0, 0, 1024, 0);
     atari_data.insert_mode_line(144, 256, 8, 0, 0, 0, 1024, 0);
 
-    atari_data.gtia1.0[0].colors[0] = 0;
-    atari_data.gtia1.0[0].colors[1] = 40;
-    atari_data.gtia1.0[0].colors[2] = 202;
-    atari_data.gtia1.0[0].colors[3] = 148;
-    atari_data.gtia1.0[0].colors[4] = 70;
+    for scan_line in 0..240 {
+        atari_data.gtia1.0[scan_line].colors[0] = 0;
+        atari_data.gtia1.0[scan_line].colors[1] = 40;
+        atari_data.gtia1.0[scan_line].colors[2] = 202;
+        atari_data.gtia1.0[scan_line].colors[3] = 148;
+        atari_data.gtia1.0[scan_line].colors[4] = 70;
+        atari_data.gtia1.0[scan_line].colors[5] = 0;
+        atari_data.gtia1.0[scan_line].colors[6] = 0;
+        atari_data.gtia1.0[scan_line].colors[7] = 0;
+
+        atari_data.gtia1.0[scan_line].colors_pm[0] = 0x2a;
+        atari_data.gtia1.0[scan_line].colors_pm[1] = 0x4a;
+        atari_data.gtia1.0[scan_line].colors_pm[2] = 0x6a;
+        atari_data.gtia1.0[scan_line].colors_pm[3] = 0x8a;
+
+        atari_data.gtia3.0[scan_line].hposp[0] = 64.0 - 4.0;
+        atari_data.gtia3.0[scan_line].hposp[1] = 96.0 - 4.0;
+        atari_data.gtia3.0[scan_line].hposp[2] = 128.0 - 4.0;
+        atari_data.gtia3.0[scan_line].hposp[3] = 160.0 - 4.0;
+        atari_data.gtia3.0[scan_line].hposm = [192.0 - 4.0, 194.0 - 4.0, 196.0 - 4.0, 198.0 - 4.0];
+        atari_data.gtia3.0[scan_line].grafm = 0x55;
+        atari_data.gtia3.0[scan_line].prior = 4;
+
+        atari_data.gtia2.0[scan_line].player_size[0] = 16.0;
+        atari_data.gtia2.0[scan_line].player_size[1] = 16.0;
+        atari_data.gtia2.0[scan_line].player_size[2] = 16.0;
+        atari_data.gtia2.0[scan_line].player_size[3] = 16.0;
+        atari_data.gtia2.0[scan_line].missile_size = [4.0, 4.0, 4.0, 4.0];
+
+        atari_data.gtia2.0[scan_line].grafp[0] = 0x55;
+        atari_data.gtia2.0[scan_line].grafp[1] = 0x55;
+        atari_data.gtia2.0[scan_line].grafp[2] = 0x55;
+        atari_data.gtia2.0[scan_line].grafp[3] = 0x55;
+    }
 
     let mem = atari_data.reserve_antic_memory(1024);
     mem.copy_from_slice(include_bytes!("charset.dat"));
 
-    atari_data.reserve_antic_memory(40).copy_from_slice(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]);
-    atari_data.reserve_antic_memory(40).copy_from_slice(&[0, 0, 50, 101, 97, 100, 121, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]);
-    atari_data.reserve_antic_memory(40).copy_from_slice(&[0, 0, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]);
+    atari_data.reserve_antic_memory(40).copy_from_slice(&[
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+    atari_data.reserve_antic_memory(40).copy_from_slice(&[
+        0, 0, 50, 101, 97, 100, 121, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+    atari_data.reserve_antic_memory(40).copy_from_slice(&[
+        0, 0, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
 
     let mesh = atari_data.create_mesh();
 
